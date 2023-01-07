cdabd5854da5dea448dbabb4a482583560418af2
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2023-01-07 10:12:50 +0100

    encapsulation

---
+++ src/Person.java
@@ -0,0 +1,57 @@
+import java.time.Year;
+
+public class Person {
+    private String identifier;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int birthYear;
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getAge() {
+        return Year.now().getValue() - birthYear;
+    }
+
+    public boolean equals(Person other) {
+        return identifier.equals(other.identifier);
+    }
+}
