3bbe5a2432efee3b1564f30b9dd3e7a477645672
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2023-01-07 09:17:20 +0100

    encapsulation

---
+++ src/ProductEncapsulation.java
@@ -0,0 +1,34 @@
+public class ProductEncapsulation {
+    private String number;
+    private String label;
+    private double price;
+    private double stock;
+
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getStock() {
+        return stock;
+    }
+    public void setStock(double stock) {
+        this.stock = stock;
+    }
+}
