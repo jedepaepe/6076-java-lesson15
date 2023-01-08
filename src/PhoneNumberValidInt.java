1cc7a8b276b8b5fe824fa94e1cd0aeaacfee9501
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2023-01-08 10:40:51 +0100

    encapsulation

---
+++ src/PhoneNumberValidInt.java
@@ -0,0 +1,19 @@
+public class PhoneNumberValidInt {
+    private int value;
+
+    public String getValue() {
+        return "" + value;
+    }
+
+    public void setValue(String value) {
+        if (value == null) {
+            throw new IllegalArgumentException("la valeur ne peut pas êtr nulle");
+        }
+        for (int i = 0; i < value.length(); ++i) {
+            if ('0' > value.charAt(i) || value.charAt(i) > '9') {
+                throw new IllegalArgumentException(value + " ne peut contenir que des chiffres");
+            }
+        }
+        this.value = Integer.parseInt(value);
+    }
+}
